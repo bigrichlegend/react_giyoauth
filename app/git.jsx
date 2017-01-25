03076bef5ce0c6efd4735b5d9445bf828a36913a
Richard McPherson <bigrichlegend@ymail.com>
2017-01-25 18:16:07 +0000

    updates to oauth login process

--- app/git.jsx
+++ app/git.jsx
@@ -2,14 +2,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HomePage from './components/Home/Home';
-import HeaderComponent from 'app/components/Home//Header';
+import MyApp from './components/MyApp';
+import HeaderComponent from 'app/components/Home/Header';
 
 ReactDOM.render(
 	<HeaderComponent />,
 	document.getElementById('appheader')
 );
 
+let page ;
+
+if(localStorage.getItem('idToken') !== null){	
+	ReactDOM.render(
+		<MyApp />,
+		document.getElementById('app')
+	);
+}else{	
+	ReactDOM.render(
+		<HomePage />,
+		document.getElementById('app')
+	);
+}
+/*
 ReactDOM.render(
-	<HomePage/>,
+
+	{page},
 	document.getElementById('app')
 );
+*/
+//<HomePage/>,
